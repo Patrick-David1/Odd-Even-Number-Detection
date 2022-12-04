d7a1cea6519848a9fc53647efd899f12acf2db77
Patrick David <82005507+Patrick-David1@users.noreply.github.com>
2022-12-04 19:38:40 +0800

    Add files via upload

---
+++ Clean.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if(num % 2 == 0) {
+        cout << num << " Is an even number.\n";
+    } else {
+        cout << num << " Is an odd number.\n";
+    }
+}
+
+//basic math lol//
